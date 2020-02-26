597e1937ab3bc2c85dc08be59ca30295fad25d82
JaebinChoi <choijaebin13@gmail.com>
2020-02-26 20:58:24 +0900

    update 0226

--- JUNGOL/AdvancedCoder/Main_1863_종교.java
+++ JUNGOL/AdvancedCoder/Main_1863_종교.java
@@ -1,4 +1,4 @@
-package com.ssafy.graph;
+package jungol;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -7,10 +7,6 @@ import java.util.StringTokenizer;
 
 public class Main_1863_종교 {
 	static int[] parent;
-
-	public static void makeSet(int v) {
-		parent[v] = v;
-	}
 
 	public static int findSet(int v) {
 		if (parent[v] == v) { // root인지 검사
@@ -27,36 +23,50 @@ public class Main_1863_종교 {
 	}
 
 	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("res/종교.txt"));
+		// System.setIn(new FileInputStream("res/종교.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String line = br.readLine();
 		StringTokenizer st = new StringTokenizer(line);
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		parent = new int[n + 1];
+
+		int n = Integer.parseInt(st.nextToken()); // 학생 수
+		int m = Integer.parseInt(st.nextToken()); // 쌍의 수
+
+		parent = new int[n + 1]; // 부모를 나타내는 배열
 		int count = 0;
+
+		// 각 인덱스의 부모를 본인으로 설정
+		for (int idx = 1; idx <= n; idx++) {
+			parent[idx] = idx;
+		}
 
 		for (int tc = 0; tc < m; tc++) {
 			line = br.readLine();
 			st = new StringTokenizer(line);
+
 			int i = Integer.parseInt(st.nextToken());
 			int j = Integer.parseInt(st.nextToken());
 
-			if (parent[i] == 0)
-				makeSet(i);
-			if (parent[j] == 0)
-				makeSet(j);
+			// 큰 수의 부모를 작은 수로 설정
+			if (i < j)
+				union(j, i);
+			else
+				union(i, j);
 
-			union(i, j);
 		}
 
-		for (int idx = 1; idx < n; idx++) {
+		// 본인이 부모인 개수 파악
+		for (int idx = 1; idx < n + 1; idx++) {
 			if (parent[idx] == idx) {
 				count++;
 			}
 		}
-		
+
+		// 쌍이 없으면 종교 수는 학생 수
+		if (m == 0) {
+			count = n;
+		}
+
 		System.out.println(count);
-		
+
 	}
 }
