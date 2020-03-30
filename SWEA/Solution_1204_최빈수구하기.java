577524aa41411e1083ffa622201eb15333c32159
JaebinChoi <choijaebin13@gmail.com>
2020-03-30 20:41:51 +0900

    0330

--- SWEA/Solution_1204_최빈수구하기.java
+++ SWEA/Solution_1204_최빈수구하기.java
@@ -1,36 +1,38 @@
-package swea;
-import java.util.Scanner;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
- 
-class Solution {
-    public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T = sc.nextInt();
- 
-        for (int test_case = 1; test_case <= T; test_case++) {
-            int index = sc.nextInt();
-             
-            int score[] = new int[1001];
-            int score_count[] = new int[101];
-            int fre = 0;
-            int max = 0;
- 
-            for (int i = 1; i < 1001; i++) {
-                score[i] = sc.nextInt();
-                score_count[score[i]] += 1;
-            }
- 
-            fre = score_count[1];
-            for (int j = 2; j < 101; j++) {
-                if (score_count[j] >= fre) {
-                    fre = score_count[j];
-                    max = j;
-                }
-            }
- 
-            System.out.println("#" + index + " " + max);
-        }
- 
-    }
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_1204_최빈수구하기 {
+	static int[] scores;
+	static int frequency; // 제일 빈도가 많은 수
+
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("res/최빈수.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			scores = new int[101];
+			frequency = Integer.MIN_VALUE;
+			br.readLine();
+
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+			for (int i = 0; i < 1000; i++) {
+				scores[Integer.parseInt(st.nextToken())]++;
+			}
+
+			int max = 0;
+			for (int i = 0; i < 101; i++) {
+				if (scores[i] >= frequency) {
+					frequency = scores[i];
+					max = i;
+				}
+			}
+
+			System.out.println("#" + tc + " " + max);
+		}
+	}
 }
