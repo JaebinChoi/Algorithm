54fae137bb09e871be51ec04a075f70c28f888a2
JaebinChoi <choijaebin13@gmail.com>
2020-06-08 19:44:20 +0900

    0608

--- Baekjoon/Main_1987_알파벳.java
+++ Baekjoon/Main_1987_알파벳.java
@@ -1,57 +1,52 @@
-package backjoon;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main_1987_알파벳 {
-	static int R, C;
+	static int R, C, result;
 	static char[][] map;
-	static ArrayList<Character> list = new ArrayList<>();
-	static int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-	static int count = 1;
-	static int result;
-
+	static Set<Character> set = new HashSet<>();
+	static int[][] dir = {{-1, 0}, {1, 0}, {0 ,-1}, {0, 1}};
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		map = new char[R][C];
-
-		for (int i = 0; i < R; i++) {
+		
+		for(int i = 0; i < R; i++) {
 			String line = br.readLine();
-			for (int j = 0; j < C; j++) {
+			for(int j = 0; j < C; j++) {
 				map[i][j] = line.charAt(j);
 			}
 		}
-
-		list.add(map[0][0]); // 첫 시작 알파벳 list에 추가
-		dfs(0, 0); // 탐색 시작
-
+		
+		set.add(map[0][0]);
+		dfs(0, 0, 1);
+		
 		System.out.println(result);
 	}
 
-	private static void dfs(int r, int c) {
-		int nr, nc;
-
-		for (int i = 0; i < 4; i++) {
-			nr = r + dir[i][0];
-			nc = c + dir[i][1];
-
-			if (nr > -1 && nr < R && nc > -1 && nc < C && !list.contains(map[nr][nc])) { // 리스트에 알파벳이 없으면
-				count++;
-				list.add(map[nr][nc]);
-
-				dfs(nr, nc);
-
-				count--;
-				list.remove(list.indexOf(map[nr][nc]));
-			}
+	private static void dfs(int r, int c, int cnt) {
+		result = result > cnt ? result : cnt;
+		
+		for(int i = 0; i < 4; i++) {
+			int nr = r + dir[i][0];
+			int nc = c + dir[i][1];
+			
+			if(!isIn(nr, nc) || set.contains(map[nr][nc]))
+				continue;
+			
+			set.add(map[nr][nc]);
+			dfs(nr, nc, cnt + 1);
+			set.remove(map[nr][nc]);
 		}
-
-		result = Math.max(result, count);
 	}
 
+	private static boolean isIn(int nr, int nc) {
+		return nr >= 0 && nc >= 0 && nr < R && nc < C;
+	}
 }
