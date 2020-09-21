260011d404e89d48e93c793b3640909c21636b0c
JaebinChoi <choijaebin13@gmail.com>
2020-09-21 19:14:22 +0900

    0921

--- Programmers/Solution_두정수사이의합.java
+++ Programmers/Solution_두정수사이의합.java
@@ -1,19 +1,29 @@
+package complete;
 class Solution_두정수사이의합 {
 	public long solution(int a, int b) {
-		int from = a < b ? a : b;
-		int to = a < b ? b : a;
-		
-		int sum = 0;
-		for(int i = from; i <= to; i++)
-			sum += i;
-		
-		return sum;
+		long answer = 0;
+
+		long from = a < b ? a : b;
+		long to = a < b ? b : a;
+
+		if (from >= 0 && to >= 0)
+			answer = (to * (to + 1) / 2) - ((from - 1) * from / 2);
+		else if (from < 0 && to >= 0) {
+			from = Math.abs(from);
+			answer = (to * (to + 1) / 2) - (from * (from + 1) / 2);
+		} else {
+			from = Math.abs(from);
+			to = Math.abs(to);
+			answer = -1 * ((from * (from + 1) / 2) - ((to - 1) * to / 2));
+		}
+
+		return answer;
 	}
 
 	public static void main(String[] args) {
-		int a = -5;
-		int b = 3;
+		int a = -3;
+		int b = -5;
 
-		System.out.println(new Solution_두정수사이의합().solution(a, b));
+		new Solution_두정수사이의합().solution(a, b);
 	}
 }
