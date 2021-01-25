f327c416fc5becff847ae27b29197b22f1fc00d7
JaebinChoi <choijaebin13@gmail.com>
2021-01-25 17:53:36 +0900

    20210125

---
+++ Baekjoon/Main_1912_연속합.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main_1912_연속합 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], 0) + arr[i];
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int num : dp) {
+            max = Math.max(max, num);
+        }
+
+        System.out.println(max);
+    }
+}
