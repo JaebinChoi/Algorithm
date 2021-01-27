120bf710c0db6eaa964cddaba0c0c9b0610607fd
JaebinChoi <choijaebin13@gmail.com>
2021-01-27 11:58:17 +0900

    20210127

---
+++ Baekjoon/Main_17219_비밀번호찾기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main_17219_비밀번호찾기 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            String addr = st.nextToken();
+            String pw = st.nextToken();
+            map.put(addr, pw);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            sb.append(map.get(br.readLine())).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
